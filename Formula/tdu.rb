c93b571488f9de9dc65005564ede56fc6b5ce4b7
Anatoli Babenia <anatoli@rainforce.org>
2021-08-04 08:34:03 +0300

    Proper hompage URL

--- Formula/tdu.rb
+++ Formula/tdu.rb
@@ -1,6 +1,6 @@
 class Tdu < Formula
   desc "Top Disk Usage"
-  homepage "https://github.com/josephpaul0/tdu/src/"
+  homepage "https://github.com/josephpaul0/tdu/"
   url "https://bitbucket.org/josephpaul0/tdu/get/v1.36.zip"
   sha256 "84bd93722527c62d74f6bd2bb00e078502ebb30b8c451229858999f000880ff1"
   license "GPL-2.0-only"
