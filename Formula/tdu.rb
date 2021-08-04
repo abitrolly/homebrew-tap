aea8078fa255ec1e270b9358edaa5348bb4d0b43
Anatoli Babenia <anatoli@rainforce.org>
2021-08-04 08:33:13 +0300

    1.36 supports modules natively and has new home

--- Formula/tdu.rb
+++ Formula/tdu.rb
@@ -1,6 +1,6 @@
 class Tdu < Formula
   desc "Top Disk Usage"
-  homepage "https://bitbucket.org/josephpaul0/tdu/src/master/"
+  homepage "https://github.com/josephpaul0/tdu/"
   url "https://bitbucket.org/josephpaul0/tdu/get/v1.36.zip"
   sha256 "84bd93722527c62d74f6bd2bb00e078502ebb30b8c451229858999f000880ff1"
   license "GPL-2.0-only"
@@ -8,7 +8,6 @@ class Tdu < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GO111MODULE"] = "auto"
     system "go", "build", *std_go_args
   end
 
