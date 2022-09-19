a502dad94965373b9d1f910dc2524e8f24e54d2e
Anatoli Babenia <anatoli@rainforce.org>
2022-09-19 04:16:05 +0300

    Sorbet (new formula)

---
+++ Formula/sorbet.rb
@@ -0,0 +1,30 @@
+class Sorbet < Formula
+  desc "Fast, powerful type checker designed for Ruby"
+  homepage "https://sorbet.org"
+  url "https://github.com/sorbet/sorbet/archive/refs/tags/0.5.10439.20220916051958-58983475a.tar.gz"
+  sha256 "ef751bd209c2bc9a35d91e1d4b391489ab8a51e9319e2113bcc0eace1cd0cf7b"
+  license "Apache-2.0"
+
+  uses_from_macos "ruby"
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "#{name}.gemspec"
+    system "gem", "install", "#{name}-#{version}.gem"
+    bin.install libexec/"bin/#{name}"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test sorbet`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
